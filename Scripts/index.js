573f15139512cf71ff6ce2a3d7ad2dbdc1c9911a
Carlos Eduardo <cedut.dev@gmail.com>
2024-06-24 12:36:40 +0000

    feat: Adiciona página de Projetos e modificações no botão de tema
    - Refatora o botão de tema para melhorar usabilidade
    - Ajusta a exibição dos links nos contatos para maior clareza
    - Realiza outras pequenas melhorias no código e na interface

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,12 +1,13 @@
 const container = document.querySelector(".container");
 const themeButton = document.querySelector(".themeButton");
 const divs = document.querySelectorAll(".content");
+const projectDivs = document.querySelectorAll(".projectContent");
 
 themeButton.addEventListener("click", () => {
   if (container.classList[1] == undefined) {
-    themeButton.innerText = "TEMA CLARO";
+    themeButton.innerText = "light_mode";
   } else {
-    themeButton.innerText = "TEMA ESCURO";
+    themeButton.innerText = "dark_mode";
   }
 
   container.classList.toggle("darkTheme");
@@ -21,4 +22,14 @@ function display(className) {
   content.style.display = "block";
 }
 
+function displayProjects(className) {
+  const content = document.querySelector(className);
+  for (const div of projectDivs) {
+    div.style.display = "none";
+  }
+
+  content.style.display = "block";
+}
+
 display(".home");
+displayProjects(".project01");
